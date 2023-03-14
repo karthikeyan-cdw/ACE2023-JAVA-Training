99b8099cea9347d27aa5429208f27825611267a8
karthikeyan-cdw <karthikeyan.chokalingam@cdw.com>
2023-03-14 19:30:12 +0530

    Changed package names

---
+++ assignment-1/Q105/Main.java
@@ -0,0 +1,25 @@
+package Q105;
+
+public class Main {
+    public static void main(String[] args) {
+        C c = new C();
+        System.out.println(c);
+    }
+}
+
+class A {
+    A() {
+        System.out.println("Constructor of A called");
+    }
+}
+
+class B {
+    B() {
+        System.out.println("Constructor of B called");
+
+    }
+}
+
+class C extends A {
+    B b = new B();
+}
